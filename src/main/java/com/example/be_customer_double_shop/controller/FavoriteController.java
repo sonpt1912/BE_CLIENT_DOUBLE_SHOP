da4095073666755b82a9498398fc09219b11b136
Pham Thanh son (FIS PBDXGHN) <sonpt51@fpt.com>
2024-04-14 14:52:01 +0700

    them phan cart

--- src/main/java/com/example/be_customer_double_shop/controller/FavoriteController.java
+++ src/main/java/com/example/be_customer_double_shop/controller/FavoriteController.java
@@ -6,4 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/favorite")
 public class FavoriteController {
+
+    // add to favorite
+
+    // delete from favorite
+
+
+
 }
---
+++ src/main/java/com/example/be_customer_double_shop/repository/FavoriteRepository.java
@@ -0,0 +1,7 @@
+package com.example.be_customer_double_shop.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoriteRepository {
+}
