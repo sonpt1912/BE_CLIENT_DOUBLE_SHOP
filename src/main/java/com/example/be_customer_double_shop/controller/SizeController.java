60e720da6cd325c14d139a58a068b221f32a49bc
sonpt <phamson191203@gmail.com>
2024-05-03 15:00:58 +0700

    fix lai cai api dang su dung @service -> @RestController

--- src/main/java/com/example/be_customer_double_shop/controller/SizeController.java
+++ src/main/java/com/example/be_customer_double_shop/controller/SizeController.java
@@ -7,8 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
 @RequestMapping("/size")
 public class SizeController {
 
---
+++ src/main/java/com/example/be_customer_double_shop/service/Impl/ColorServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.be_customer_double_shop.service.Impl;
+
+import com.example.be_customer_double_shop.service.ColorService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ColorServiceImpl implements ColorService {
+}
