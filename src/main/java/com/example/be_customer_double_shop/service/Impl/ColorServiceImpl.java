98be0c67abf9f3b54c331376c87976693cae7406
sonpt <phamson191203@gmail.com>
2024-05-04 07:36:01 +0700

    fix api color and size

--- src/main/java/com/example/be_customer_double_shop/service/Impl/ColorServiceImpl.java
+++ src/main/java/com/example/be_customer_double_shop/service/Impl/ColorServiceImpl.java
@@ -1,8 +1,22 @@
 package com.example.be_customer_double_shop.service.Impl;
 
+import com.example.be_customer_double_shop.entity.Color;
+import com.example.be_customer_double_shop.repository.ColorRespository;
 import com.example.be_customer_double_shop.service.ColorService;
+import com.example.be_customer_double_shop.util.Constant;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ColorServiceImpl implements ColorService {
+
+    @Autowired
+    private ColorRespository colorRespository;
+
+    @Override
+    public List<Color> getAllByStatus() {
+        return colorRespository.findAllByStatus(Constant.ACTIVE);
+    }
 }
