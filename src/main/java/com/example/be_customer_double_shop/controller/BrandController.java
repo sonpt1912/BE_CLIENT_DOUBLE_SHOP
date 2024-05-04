910dceb35ee68483533f6676e568c214fbdf9bdc
sonpt <phamson191203@gmail.com>
2024-05-04 21:11:56 +0700

    fix api chuyen sang public

--- src/main/java/com/example/be_customer_double_shop/controller/BrandController.java
+++ src/main/java/com/example/be_customer_double_shop/controller/BrandController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/brand")
+@RequestMapping("/public/brand")
 public class BrandController {
 
     @Autowired
--- src/main/java/com/example/be_customer_double_shop/controller/SizeController.java
+++ src/main/java/com/example/be_customer_double_shop/controller/SizeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/size")
+@RequestMapping("/public/size")
 public class SizeController {
 
     @Autowired
