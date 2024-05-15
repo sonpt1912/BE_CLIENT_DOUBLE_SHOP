2df79156b5addc0da2942ba7585d783c9296053d
sonpt <phamson191203@gmail.com>
2024-05-15 10:32:39 +0700

    up

--- src/main/java/com/example/be_customer_double_shop/service/Impl/DetailBillServiceImpl.java
+++ src/main/java/com/example/be_customer_double_shop/service/Impl/DetailBillServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.be_customer_double_shop.service.Impl;
 
 import com.cloudinary.Cloudinary;
+import com.example.be_customer_double_shop.dao.DetailBillDao;
 import com.example.be_customer_double_shop.dto.request.BillRequest;
 import com.example.be_customer_double_shop.entity.Bill;
 import com.example.be_customer_double_shop.entity.DetailBill;
@@ -12,13 +13,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 @Service
 public class DetailBillServiceImpl implements DetailBillService {
 
     @Autowired
     private DetailBillRepository detailBillRepository;
+
+    @Autowired
+    private ExecutorService executorService;
 
     @Autowired
     private Cloudinary cloudinary;
@@ -41,8 +51,37 @@ public class DetailBillServiceImpl implements DetailBillService {
     }
 
     @Override
-    public Object getDetailBill(BillRequest billRequest) {
-        return detailBillRepository.getDetailBillByBill(billRequest.getId());
+    public Object getDetailBill(BillRequest billRequest) throws InterruptedException, ExecutionException {
+        List<DetailBillDao> detailBillDaoList = detailBillRepository.getDetailBillByBill(billRequest.getId());
+
+        List<Callable<Map<String, Object>>> callableList = new ArrayList<>();
+
+        for (DetailBillDao detailBillDao : detailBillDaoList) {
+            Callable callable = () -> {
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", detailBillDao.getId());
+                map.put("name", detailBillDao.getName());
+                map.put("color", detailBillDao.getColor());
+                map.put("size", detailBillDao.getSize());
+                map.put("code", detailBillDao.getCode());
+                map.put("quantity", detailBillDao.getQuantity());
+                map.put("price", detailBillDao.getPrice());
+                map.put("disscountAmout", detailBillDao.getDiscountAmout());
+                map.put("listImages", cloudinary.search().expression("folder:double_shop/product/" + detailBillDao.getCode() + "/*").maxResults(500).execute());
+                return map;
+            };
+            callableList.add(callable);
+        }
+
+        List<Future<Map<String, Object>>> futureList = executorService.invokeAll(callableList);
+
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        for (Future future : futureList) {
+            list.add((Map<String, Object>) future.get());
+        }
+
+        return list;
     }
 
     @Override
