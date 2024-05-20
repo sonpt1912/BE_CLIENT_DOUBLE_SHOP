c77dc5b5398dd66ae13ef53f776cadf2ad820c2b
thanhhhph28937 <126442063+thanhhhph28937@users.noreply.github.com>
2024-05-20 23:42:06 +0700

    fix

--- src/main/java/com/example/be_customer_double_shop/controller/CustomerVoucherController.java
+++ src/main/java/com/example/be_customer_double_shop/controller/CustomerVoucherController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/customer-voucher")
+@RequestMapping("/public/customer-voucher")
 public class CustomerVoucherController {
     @Autowired
     private JwtProvider jwtProvider;
